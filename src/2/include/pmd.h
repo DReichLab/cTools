13b5f9ec6a843adf364fe1b9545f0295892a2d2f
Mengyao <zhaomengyao@gmail.com>
2014-12-09 12:04:41 -0500

    folders are cleaned

--- src/2/include/pmd.h
+++
@@ -1,8 +0,0 @@
-
-double pmd_pontus(READ *readpt, int udgtype, int pmdoff)  ;
-int setudgtype(char *mode) ;       
-void clearpmdhist(int npops) ;
-void destroypmdhist() ;
-void addpmdhist(int ipop, double y) ;
-void printpmdhist()  ;
-
