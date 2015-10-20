952c2df7016f4ca6148d6be4c3af07601bf1913d
Mengyao <zhaomengyao@gmail.com>
2015-10-20 13:57:44 -0400

    ccompress.c interface update, no comments

--- src/ccompress.c
+++ src/ccompress.c
@@ -2,7 +2,7 @@
  * ccompress.c: use razip to compress hetfa files (*.hetfa) and mask files (*.fa)
  * Author: Nick Patterson
  * Revised by: Mengyao Zhao
- * Last revise date: 2015-10-19
+ * Last revise date: 2015-10-20
  * Contact: mengyao_zhao@hms.harvard.edu 
  */
 
@@ -21,8 +21,6 @@ char *regname = NULL ;
 char *regstring = NULL ;
 faidx_t *fai;
 int chimpmode = NO ;
-//char *iubfile = "/n/data1/hms/genetics/reich/1000Genomes/cteam_remap/B-cteam_lite/v0.2/C-FullyPublic__SignedLetterNoDelay__SignedLetterDelay/C.hetfa.dblist" ;
-//char *iubmaskfile = "/n/data1/hms/genetics/reich/1000Genomes/cteam_remap/B-cteam_lite/v0.2/C-FullyPublic__SignedLetterNoDelay__SignedLetterDelay/C.mask.dblist" ;
 phandle *ph  = NULL ;
 
 void loadfilebase(char *parname) ;
@@ -31,24 +29,15 @@ char *myfai_fetch(faidx_t *fai, char *reg, int  *plen) ;
 FILE *fff ; 
 
 char *iname = "S_Irula-1" ;  
-//char *wkdir = "./" ;	// default writing directory
 char *tempout ;
 
 void readcommands(int argc, char **argv) ;
-//int setstring(char *iname, unsigned char *ketfa, unsigned char *countfa, int len)  ;
 void writefa(FILE *fff, char *regname, char *rrr)  ;
 
 static int usage()
 {
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Usage:   ccompress <reference.fa> <hetfa.fa> \n\n");
-//	fprintf(stderr, "Options:\n");
-//	fprintf(stderr, "\t-f <hetfa file> Please give absolute path with the file name.\n");
-//	fprintf(stderr, "\t-r <Href.fa> Please give absolute path with the file name.\n");
-	//fprintf(stderr, "\t-i <sample name> [default: S_Irula-1]\n");
-//	fprintf(stderr, "\t-w <output dir> [default: ./]\n");
-//	fprintf(stderr, "\t-? Show the instruction.\n\n");
-	//fprintf(stderr, "Note: If you are not working on orchestra, -i option is not applicable to you, and you need to use -f and -r options together. If you use -f and -r options, -i option will be ignored.\n\n");
 	return 1;
 }
 
@@ -102,14 +91,12 @@ char *poplist[2] ;
 
  readcommands(argc, argv);
 
-//	if (f == 1) {
 	poplist[0] = argv[1];
 	poplist[1] = argv[2];
 	p = strrchr(poplist[1], '.');
 	x = strlen(poplist[1]);
 	iname = (char*)malloc(x*sizeof(char));
 	memset (iname, '\0', sizeof(iname));
-//fprintf(stderr, "poplist[1]: %s, x:%d\n", poplist[1], x);
 	if (!strcmp(p, ".fa")) strncpy(iname, poplist[1], x - 3); 
 	else if (!strcmp(p, ".hetfa")) strncpy(iname, poplist[1], x - 6);
 	p = strrchr(iname, '/');
@@ -118,7 +105,6 @@ char *poplist[2] ;
 	file = strrchr(poplist[1], '/');
 	dir = (char*)malloc(x*sizeof(char));
 	strncpy(dir, poplist[1], x - strlen(file));
-//fprintf(stderr, "dir: %s\n", dir);
 
 	  setfalist(poplist, 2, iublist)  ;
 	  setfalist(poplist, 2, iubmask)  ;
@@ -132,11 +118,7 @@ char *poplist[2] ;
   nregs = getss(reglist, fainame) ;
   printf("regs:\n") ;
   printstringsw(reglist, nregs, 5, 10) ;
-  
 
-  //sprintf(outfaname, "%s/%s.ccomp.fa", wkdir, iname) ;  
-  
-  //sprintf(tmpfaname, "%s/%s.ccomptmp.fa", wkdir, iname) ;  
   sprintf(outfaname, "%s/%s.ccomp.fa", dir, p) ;  
   
   sprintf(tmpfaname, "%s/%s.ccomptmp.fa", dir, p) ;  
@@ -167,23 +149,17 @@ char *poplist[2] ;
  system (ss) ;  
  sprintf(ss, "mv %s.rz %s.rz", tmpfaname, outfaname) ;
  system (ss) ;  
- //sprintf(outfaname, "%s/%s.ccompmask.fa", wkdir, iname) ;  
  sprintf(outfaname, "%s/%s.ccompmask.fa", dir, p) ;  
  sprintf(ss, "cp %s %s", iubmask[1], outfaname) ; 
-//fprintf(stderr, "ss: %s\n", ss);
  system(ss) ;
  sprintf(ss, "htsbox razip  %s",  outfaname) ; 
  system(ss) ;
 
-//	sprintf(ss, "rm %s/%s.ccomptmp.fa", dir, p);
-//	sprintf(ss, "rm %s.ccomptmp.fa", p);
-//	system(ss);
   printf("## end of ccompress\n") ;
 
 	free(dir);
 	free(iname);
   return 0 ;
-
 }
 
 void writefa(FILE *fff, char *regname, char *rrr) 
@@ -243,38 +219,6 @@ void readcommands(int argc, char **argv)
   char str[512]  ;
   int n, kode ;
   int pops[2] ;
-
-/*  while ((i = getopt (argc, argv, "w:?")) != -1) {
-
-    switch (i)
-      {
-
-      case 'i':
-	iname = strdup(optarg) ;
-	break;
-
-      case 'w':
-	wkdir = strdup(optarg) ;
-	break;
-
-      case 'f':
-	//	f = 1;
-		poplist[1] = strdup(optarg) ;
-	break;
-
-      case 'r':	
-	//	f = 1;
-	poplist[0] = strdup(optarg) ;
-	break;
-
-      case '?':
-//	default:
-//	printf ("Usage: bad params.... %c\n", i) ;
-	exit(usage());
-	//fatalx("bad params flag:%c\n, i") ;
-      }
-  }*/
-//fprintf(stderr, "argc: %d\n", argc);
 	if (argc < 3) exit(usage());
 
 }
